b01d09b027ff21fe63b2d6ad91ecfc48528cd683
Alexander Susikov <a.susikov@rocketguys.com>
2020-02-06 12:50:38 +0300

    Fixed init of job

--- lib/yabeda/sidekiq/server_middleware.rb
+++ lib/yabeda/sidekiq/server_middleware.rb
@@ -8,7 +8,7 @@ module Yabeda
         labels = Yabeda::Sidekiq.labelize(worker, job, queue)
         start = Time.now
         begin
-          job_instance = Sidekiq::Job.new(job)
+          job_instance = ::Sidekiq::Job.new(job)
           Yabeda.sidekiq_job_latency.measure(labels, job_instance.latency)
           yield
           Yabeda.sidekiq_jobs_success_total.increment(labels)
